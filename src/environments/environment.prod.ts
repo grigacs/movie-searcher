448cb1d8e19bc07baf239e19f952f3245221c2d6
Gergely Der <grigacs91@gmail.com>
2019-12-17 03:14:52 +0100

    responsive design added, change name into original name, thats get from api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.themoviedb.org/3/',
+  searchMoviesEndpoint: 'search/multi',
+  apiKey: '1c5abaaeaa13c66b570ad3042a0d51f4',
+  apiLanguage: 'en-US',
+  imgAbsolutePath: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
+  imdbLink: 'http://www.imdb.com/title/'
 };
